ef756d4788369305a76accbf48722db126c126a5
andrewelamb <andrewelamb@gmail.com>
2018-02-23 10:38:55 -0800

    fixed synapse issue

--- analysis/GSE81861/upload_annotation_files_to_synapse.R
+++ analysis/GSE81861/upload_annotation_files_to_synapse.R
@@ -2,20 +2,35 @@ library(plyr)
 library(doMC)
 library(tidyverse)
 library(synapser)
-library(data.table)
 library(magrittr)
 
-home_dir <- "/home/aelamb/repos/Tumor-Deconvolution-Challenge/"
-tmp_dir  <- "/home/aelamb/tmp/"
-download_path <- "/tmp/mozilla_aelamb0/sanitized_annotations"
+home_dir      <- "/home/aelamb/repos/Tumor-Deconvolution-Challenge/"
+tmp_dir       <- "/home/aelamb/tmp/"
+download_path <- "/tmp/mozilla_aelamb0/sanitized_annotations/"
 upload_id     <- "syn11867744"
-
-source_url    <-  "https://www.dropbox.com/s/ky8t1yc17s40qz8/Archive.zip?dl=0"
-
 
 
 setwd(home_dir)
 source("scripts/utils.R")
 setwd(tmp_dir)
-synLogin()
+
 registerDoMC(cores = detectCores())
+
+files <- download_path %>% 
+    list.files(full.name = T)
+
+activity_obj <- Activity(
+    name = "upload",
+    description = "upload unziped sanitized annotations after downloading from dropbox",
+    used = list("https://www.dropbox.com/s/ky8t1yc17s40qz8/Archive.zip?dl=0"),
+    executed = list("https://github.com/Sage-Bionetworks/Tumor-Deconvolution-Challenge/blob/master/analysis/GSE81861/upload_annotation_files_to_synapse.R")
+)
+
+synLogin()
+l_ply(files, upload_file_to_synapse, upload_id, activity_obj = activity_obj, .parallel = T)
+
+
+
+    
+
+
