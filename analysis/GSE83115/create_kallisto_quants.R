acb62f32035e0cb860dd7a35cb3735c750498bd6
Ubuntu <ubuntu@ip-172-31-34-174.ec2.internal>
2018-05-03 00:50:20 +0000

    fix bugs

--- analysis/GSE83115/create_kallisto_quants.R
+++ analysis/GSE83115/create_kallisto_quants.R
@@ -2,16 +2,12 @@ library(tidyverse)
 library(doMC)
 
 #ec2
-home_dir  <- "/home/ubuntu/Tumor-Deconvolution-Challenge/"
 tmp_dir   <- "/home/ubuntu/tmp/"
 kallisto_dir <- "/home/ubuntu/kallisto_cwl/"
-fastq_file <- "/tmp/fastq.tsv"
-index_file <- "GRCH38.idx"    
+fastq_file <- "fastq.tsv"
+index_file <- "../GRCH38.idx"    
 
-setwd(home_dir)
-source("scripts/utils.R")
 setwd(tmp_dir)
-synLogin()
 n_cores = detectCores() - 1
 
 
@@ -26,15 +22,17 @@ run_kallisto_cwl <- function(df){
                          str_c(kallisto_dir, "quant.cwl"),
                          "--index", index_file,
                          "--threads", n_cores,
-                         "--fastq1", df$path[[1]],
-                         "--fastq2", df$path[[2]], 
+                         "--fastq1", df$paths[[1]],
+                         "--fastq2", df$paths[[2]], 
                          sep = " ")
+    print(cwl_command)
     system(cwl_command)
     cwl_command2 <- str_c("cwltool", 
                           str_c(kallisto_dir, "h5dump.cwl"),
                           "--h5 abundance.h5",
                           sep = " ")
     system(cwl_command2)
+    print(cwl_command2)
     abundance_file <- str_c(df$sample_name[[1]], "_kallisto_abundance.tsv")
     file.rename("abundance.tsv", abundance_file)
     walk(c("abundance.h5", "run_info.json"), file.remove)
