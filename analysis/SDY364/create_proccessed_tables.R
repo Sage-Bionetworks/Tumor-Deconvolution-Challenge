09b9d1914f4ef04ef0906a1470cb34c6bce1aa9a
andrewelamb <andrewelamb@gmail.com>
2019-01-14 10:16:42 -0800

    first commit

---
+++ analysis/SDY364/create_proccessed_tables.R
@@ -0,0 +1,69 @@
+library(tidyverse)
+library(synapser)
+library(data.table)
+library(magrittr)
+library(ImmuneSpaceR)
+
+# upload_id     <- "syn17096664"
+# gt_upload_id  <- "syn17096663"
+
+source("../../scripts/utils.R")
+synLogin()
+
+blood_count_id <- "syn13363369"
+expr_id  <- "syn13363367"
+
+
+connection <- ImmuneSpaceR::CreateConnection("SDY364")
+fcs <- connection$getDataset("fcs_analyzed_result")
+hai <- connection$getDataset("hai")
+
+
+blood_count_df <- blood_count_id %>% 
+    create_df_from_synapse_id %>% 
+    filter(study_accession == "SDY364") %>% 
+    select(-study_accession) %>% 
+    dplyr::rename(sample = subject_accession)
+
+columns_with_data <- blood_count_df %>% 
+    is.na() %>% 
+    colSums() %>% 
+    .[. < 23] %>% 
+    names
+
+expr_df <- expr_id  %>% 
+    download_from_synapse %>%
+    fread %>% 
+    data.frame %>% 
+    filter(study_accession == "SDY364") %>% 
+    dplyr::select(-c(study_accession, data_accession, age, gender, race)) %>% 
+    dplyr::rename(sample = subject_accession)
+
+# samples_in_common <- intersect(cytof_df$sample, expr_df$sample)
+# 
+# expr_df <- expr_df %>%
+#     filter(sample %in% samples_in_common) 
+# 
+# ground_truth_df <- cytof_df %>%
+#     dplyr::select(-c(age, gender, race)) %>%
+#     filter(sample %in% samples_in_common)
+# 
+# annotation_df <- cytof_df %>%
+#     dplyr::select(sample, age, gender, race) %>%
+#     filter(sample %in% samples_in_common)
+# 
+# activity_obj <- Activity(
+#     name = "create",
+#     description = "process GEO data into usable tables",
+#     used = list(),
+#     executed = list("https://github.com/Sage-Bionetworks/Tumor-Deconvolution-Challenge/blob/master/analysis/SDY364/create_processed_tables.R")
+# )
+# 
+# write_tsv(expr_df, "expression.tsv")
+# write_tsv(ground_truth_df, "ground_truth.tsv")
+# write_tsv(annotation_df, "annotation.tsv")
+# 
+# upload_file_to_synapse("expression.tsv", upload_id, activity_obj = activity_obj)
+# upload_file_to_synapse("annotation.tsv", upload_id, activity_obj = activity_obj)
+# upload_file_to_synapse("ground_truth.tsv", gt_upload_id, activity_obj = activity_obj)
+# 
