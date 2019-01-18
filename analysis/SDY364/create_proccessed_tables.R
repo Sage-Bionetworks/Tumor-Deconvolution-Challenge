40bda212e50111e83cd6be78b18c17c22d7c7e8b
andrewelamb <andrewelamb@gmail.com>
2019-01-18 13:59:21 -0800

    first commit

--- analysis/SDY364/create_proccessed_tables.R
+++ analysis/SDY364/create_proccessed_tables.R
@@ -3,9 +3,10 @@ library(synapser)
 library(data.table)
 library(magrittr)
 library(ImmuneSpaceR)
+library(synapserutils)
 
-# upload_id     <- "syn17096664"
-# gt_upload_id  <- "syn17096663"
+upload_id     <- "syn17971758"
+gt_upload_id  <- "syn17971750"
 
 source("../../scripts/utils.R")
 synLogin()
@@ -15,15 +16,33 @@ expr_id  <- "syn13363367"
 
 
 connection <- ImmuneSpaceR::CreateConnection("SDY364")
-fcs <- connection$getDataset("fcs_analyzed_result")
-hai <- connection$getDataset("hai")
+fcs_df <- 
+    connection$getDataset("fcs_analyzed_result") %>% 
+    dplyr::as_tibble() %>% 
+    dplyr::filter(study_time_collected == 0) %>% 
+    mutate(sample = str_sub(participant_id, end = -5)) %>% 
+    select(-c(
+        participant_id,
+        age_reported, 
+        gender,
+        race, 
+        cohort, 
+        study_time_collected, 
+        study_time_collected_unit,
+        study_time_collected_unit, 
+        base_parent_population)) %>% 
+    select(sample, everything())
+        
+    
 
 
 blood_count_df <- blood_count_id %>% 
     create_df_from_synapse_id %>% 
     filter(study_accession == "SDY364") %>% 
-    select(-study_accession) %>% 
+    select(-c(study_accession, WBC_K_per_uL)) %>% 
     dplyr::rename(sample = subject_accession)
+
+
 
 columns_with_data <- blood_count_df %>% 
     is.na() %>% 
@@ -31,39 +50,109 @@ columns_with_data <- blood_count_df %>%
     .[. < 23] %>% 
     names
 
+blood_count_df <- select(blood_count_df, columns_with_data)
+
+ground_truth_df <- blood_count_df %>% 
+    dplyr::select(-c(age, race, gender)) %>% 
+    gather(key = "celltype", value = "percent", -sample) %>% 
+    mutate(fraction = percent / 100) %>% 
+    mutate(celltype = str_remove_all(celltype, "_percent")) %>% 
+    select(-percent) %>% 
+    spread(key = "celltype", value = "fraction")
+    
+
+annotation_df <- blood_count_df %>% 
+    select(sample, age, race, gender) 
+
+
+
+
 expr_df <- expr_id  %>% 
     download_from_synapse %>%
     fread %>% 
     data.frame %>% 
-    filter(study_accession == "SDY364") %>% 
+    dplyr::filter(study_accession == "SDY364") %>% 
     dplyr::select(-c(study_accession, data_accession, age, gender, race)) %>% 
-    dplyr::rename(sample = subject_accession)
+    dplyr::rename(sample = subject_accession) %>% 
+    as_tibble() %>% 
+    gather(key = "gene", value = "expr", - sample) %>% 
+    drop_na()
 
-# samples_in_common <- intersect(cytof_df$sample, expr_df$sample)
-# 
-# expr_df <- expr_df %>%
-#     filter(sample %in% samples_in_common) 
-# 
-# ground_truth_df <- cytof_df %>%
-#     dplyr::select(-c(age, gender, race)) %>%
-#     filter(sample %in% samples_in_common)
-# 
-# annotation_df <- cytof_df %>%
-#     dplyr::select(sample, age, gender, race) %>%
-#     filter(sample %in% samples_in_common)
-# 
-# activity_obj <- Activity(
-#     name = "create",
-#     description = "process GEO data into usable tables",
-#     used = list(),
-#     executed = list("https://github.com/Sage-Bionetworks/Tumor-Deconvolution-Challenge/blob/master/analysis/SDY364/create_processed_tables.R")
-# )
-# 
-# write_tsv(expr_df, "expression.tsv")
-# write_tsv(ground_truth_df, "ground_truth.tsv")
-# write_tsv(annotation_df, "annotation.tsv")
-# 
-# upload_file_to_synapse("expression.tsv", upload_id, activity_obj = activity_obj)
-# upload_file_to_synapse("annotation.tsv", upload_id, activity_obj = activity_obj)
-# upload_file_to_synapse("ground_truth.tsv", gt_upload_id, activity_obj = activity_obj)
-# 
+linear_expr_df <- expr_df %>% 
+    spread(key = "gene", value = "expr")
+
+log_expr_df <- expr_df %>% 
+    mutate(expr = log2(expr +1)) %>% 
+    spread(key = "gene", value = "expr")
+
+remove(expr_df)
+
+
+samples_in_common <- intersect(ground_truth_df$sample, log_expr_df$sample)
+
+
+log_expr_df <- log_expr_df %>%
+    filter(sample %in% samples_in_common)
+
+linear_expr_df <- linear_expr_df %>%
+    filter(sample %in% samples_in_common)
+
+ground_truth_df <- ground_truth_df %>%
+    filter(sample %in% samples_in_common)
+
+annotation_df <- ground_truth_df %>%
+    filter(sample %in% samples_in_common)
+
+fcs_df <- fcs_df %>% 
+    filter(sample %in% samples_in_common)
+
+manifest_df <- tibble(
+    path = c("expression_log.tsv",
+             "expression_linear.tsv",
+             "annotation.tsv",
+             "fcs.tsv",
+             "ground_truth.tsv"),
+    parent = c(rep(upload_id, 4), gt_upload_id),
+    used = str_c(blood_count_id, expr_id, sep = ";"),
+    executed = "https://github.com/Sage-Bionetworks/Tumor-Deconvolution-Challenge/blob/master/analysis/SDY364/create_processed_tables.R",
+    activityName = "Process files from 10kimmunome.",
+    expression_type = c(rep("microarray", 2), rep(NA, 3)),
+    microarray_type = c(rep("unknown", 2), rep(NA, 3)),
+    expression_space = c("log2", "linear", rep(NA, 3))
+)
+
+manifest_df1 <- tibble(
+    path = c("expression_log.tsv",
+             "expression_linear.tsv"),
+    parent = upload_id,
+    used = str_c(blood_count_id, expr_id, sep = ";"),
+    executed = "https://github.com/Sage-Bionetworks/Tumor-Deconvolution-Challenge/blob/master/analysis/SDY364/create_processed_tables.R",
+    activityName = "Process files from 10kimmunome.",
+    expression_type = c(rep("microarray", 2)),
+    microarray_type = c(rep("unknown", 2)),
+    expression_space = c("log2", "linear")
+)
+
+manifest_df2 <- tibble(
+    path = c("annotation.tsv",
+             "fcs.tsv",
+             "ground_truth.tsv"),
+    parent = c(rep(upload_id, 2), gt_upload_id),
+    used = str_c(blood_count_id, expr_id, sep = ";"),
+    executed = "https://github.com/Sage-Bionetworks/Tumor-Deconvolution-Challenge/blob/master/analysis/SDY364/create_processed_tables.R",
+    activityName = "Process files from 10kimmunome."
+)
+
+
+write_tsv(manifest_df, "manifest1.tsv")
+write_tsv(manifest_df2, "manifest2.tsv")
+
+write_tsv(log_expr_df, "expression_log.tsv")
+write_tsv(linear_expr_df, "expression_linear.tsv")
+write_tsv(annotation_df, "annotation.tsv")
+write_tsv(fcs_df, "fcs.tsv")
+write_tsv(ground_truth_df, "ground_truth.tsv")
+
+syncToSynapse("manifest1.tsv")
+syncToSynapse("manifest2.tsv")
+
