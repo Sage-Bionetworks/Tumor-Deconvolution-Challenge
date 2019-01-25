3edd17374c280d8f18d6e4f3b3349af799eb8dd7
andrewelamb <andrewelamb@gmail.com>
2019-01-25 12:48:29 -0800

    rewrote to add annotations

--- analysis/GSE11057/create_proccessed_tables.R
+++ analysis/GSE11057/create_proccessed_tables.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(synapser)
+library(synapserutils)
 library(data.table)
 library(magrittr)
 library(GEOquery)
@@ -8,12 +9,15 @@ library(hgu133plus2.db)
 
 upload_id     <- "syn17091411"
 gt_upload_id  <- "syn17091410"
+dataset       <- "GSE11057"
+script_url    <- "https://github.com/Sage-Bionetworks/Tumor-Deconvolution-Challenge/blob/master/analysis/GSE11057/create_processed_tables.R"
+activity_name <- "process_files_from_GEO"
 
 source("../../scripts/utils.R")
 synLogin()
 
 
-gse_object <- getGEO("GSE11057", GSEMatrix = TRUE)
+gse_object <- getGEO(dataset, GSEMatrix = TRUE)
 
 geo_df <- gse_object %>% 
     extract2(1) %>% 
@@ -61,23 +65,52 @@ expr_df <- gse_object$GSE11057_series_matrix.txt.gz@assayData %>%
 
 samples_in_common <- intersect(expr_df$sample, ground_truth_df$sample)
 
-expr_df <- expr_df %>% 
+linear_expr_df <- expr_df %>% 
     filter(sample %in% samples_in_common) %>% 
+    spread(key = "sample", value = "expr")
+
+log_expr_df <- expr_df %>% 
+    filter(sample %in% samples_in_common) %>% 
+    mutate(expr = log2(expr +1)) %>% 
     spread(key = "sample", value = "expr")
 
 ground_truth_df <- ground_truth_df %>% 
     filter(sample %in% samples_in_common)
-  
-activity_obj <- Activity(
-    name = "create",
-    description = "process GEO data into usable tables",
-    used = list(),
-    executed = list("https://github.com/Sage-Bionetworks/Tumor-Deconvolution-Challenge/blob/master/analysis/GSE11057/create_processed_tables.R")
+
+
+
+manifest_df1 <- tibble(
+    path = c("expression_log.tsv",
+             "expression_linear.tsv"),
+    parent = upload_id,
+    executed = script_url,
+    activityName = activity_name,
+    dataset = dataset,
+    file_type = c(rep("expression", 2)),
+    expression_type = c(rep("microarray", 2)),
+    microarray_type = c(rep("hgu133plus2", 2)),
+    expression_space = c("log2", "linear")
 )
 
-write_tsv(expr_df, "expression_affy.tsv")
+manifest_df3 <- tibble(
+    path = "ground_truth.tsv",
+    parent = gt_upload_id,
+    executed = script_url,
+    activityName = activity_name,
+    dataset = dataset,
+    file_type = "ground truth",
+    unit = "fraction", 
+    cell_types = str_c(colnames(ground_truth_df)[-1], collapse = ";")
+)
+
+
+write_tsv(manifest_df1, "manifest1.tsv")
+write_tsv(manifest_df3, "manifest3.tsv")
+
+write_tsv(log_expr_df, "expression_log.tsv")
+write_tsv(linear_expr_df, "expression_linear.tsv")
 write_tsv(ground_truth_df, "ground_truth.tsv")
 
-upload_file_to_synapse("expression_affy.tsv", upload_id, activity_obj = activity_obj)
-upload_file_to_synapse("ground_truth.tsv", gt_upload_id, activity_obj = activity_obj)
+syncToSynapse("manifest1.tsv")
+syncToSynapse("manifest3.tsv")
 
