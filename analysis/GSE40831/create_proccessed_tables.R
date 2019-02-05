e1eaa2f9c2e28b91dda50993f1d315204d011efb
andrewelamb <andrewelamb@gmail.com>
2019-02-05 10:50:30 -0800

    added annotations

--- analysis/GSE40831/create_proccessed_tables.R
+++ analysis/GSE40831/create_proccessed_tables.R
@@ -1,27 +1,49 @@
 library(tidyverse)
 library(synapser)
+library(synapserutils)
 library(data.table)
 library(magrittr)
 library(GEOquery)
 library(AnnotationDbi)
 library(hgu133plus2.db)
+library(xlsx)
 
-upload_id     <- ""
-gt_upload_id  <- ""
+upload_id     <- "syn18139812"
+gt_id         <- "syn18139845"
 
 source("../../scripts/utils.R")
 synLogin()
 
+script_url    <- "https://github.com/Sage-Bionetworks/Tumor-Deconvolution-Challenge/blob/master/analysis/GSE40831/create_processed_tables.R"
+dataset       <- "GSE40831"
+activity_name <- "process GEO files into usable tables"
+used          <- gt_id
 
 
-gse_object <- getGEO("GSE40831", GSEMatrix = TRUE)
+
+gse_object <- getGEO(dataset, GSEMatrix = TRUE)
+
+ground_truth_df <- gt_id %>% 
+    download_from_synapse() %>% 
+    read.xlsx(sheetIndex = 1, startRow = 2, stringsAsFactors = F) %>% 
+    as_tibble() %>% 
+    dplyr::select(Reference.populations, Flow.cytometry) %>% 
+    set_colnames(c("cell_type", "percent")) %>% 
+    mutate(percent = as.double(percent)) %>% 
+    filter(!is.na(percent))
+    
+
+
 
 df <- gse_object %>% 
-    extract2(1) %>% 
+    extract2(2) %>% 
     phenoData() %>% 
     pData() %>%
     rownames_to_column("sample") %>%
-    as_data_frame 
+    as_tibble()
+
+
+
 
 # %>% 
 #     filter(characteristics_ch1.1 == "mixed cells") %>% 
@@ -37,11 +59,11 @@ query_df <-
         keys=keys(hgu133plus2.db,keytype="PROBEID"),
         columns=c("SYMBOL"),
         keytype="PROBEID") %>%
-    as_data_frame() %>%
+    as_tibble() %>%
     set_colnames(c("AFFY", "Hugo")) %>%
     drop_na()
 
-expr_df1 <- gse_object$`GSE40831-GPL570_series_matrix.txt.gz`@assayData %>%
+expr_df <- gse_object$`GSE40831-GPL571_series_matrix.txt.gz`@assayData %>%
     assayDataElement('exprs') %>%
     matrix_to_df("AFFY") %>%
     inner_join(query_df) %>%
