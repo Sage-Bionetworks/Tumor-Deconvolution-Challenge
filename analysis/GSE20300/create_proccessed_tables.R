9aa904aa8c136d4d3a26b61cfbe7bf02accd2c1a
Ubuntu <ubuntu@ip-172-31-11-157.ec2.internal>
2019-02-12 02:32:34 +0000

    Added annotations for GSE20300

--- analysis/GSE20300/create_proccessed_tables.R
+++ analysis/GSE20300/create_proccessed_tables.R
@@ -1,11 +1,11 @@
 library(tidyverse)
 library(synapser)
+library(synapserutils)
 library(data.table)
 library(magrittr)
 library(GEOquery)
-library(xlsx)
 library(AnnotationDbi)
-library(hgu133plus2.db)
+library(hugene11sttranscriptcluster.db)
 
 ground_truth_id <- "syn17089680"
 
@@ -15,15 +15,25 @@ gt_upload_id  <- "syn17026188"
 source("../../scripts/utils.R")
 synLogin()
 
+dataset       <- "GSE20300"
+github.path   <- "https://github.com/Sage-Bionetworks/Tumor-Deconvolution-Challenge/blob/master/analysis/"
+dataset.path  <- paste0(github.path, dataset)
+script_url    <- paste0(dataset.path, "/create_processed_tables.R")
+used          <- NA
+activity_name <- "process GEO data into usable tables"
+
+library(xlsx)
 
 ground_truth_df <- ground_truth_id %>% 
     download_from_synapse() %>% 
     xlsx::read.xlsx(1)
 
-gse <- getGEO("GSE20300", GSEMatrix = TRUE)
+gse_object <- getGEO(dataset, GSEMatrix = TRUE)
 
-series_df <-
-    pData(phenoData(gse[[1]])) %>%
+series_df <- gse_object %>% 
+    extract2(1) %>% 
+    phenoData() %>% 
+    pData() %>%
     rownames_to_column("sample") %>%
     as_data_frame %>% 
     arrange(`transplant state:ch1`)
@@ -41,50 +51,138 @@ annotation_df <- series_df %>%
     dplyr::select(sample, `transplant state:ch1`) %>% 
     dplyr::rename(transplant_state = `transplant state:ch1`)
 
+expr <-
+    gse_object %>%
+    extract2(1) %>%
+    assayDataElement('exprs') %>%
+    as.data.frame()
 
+gpl <- getGEO(gse_object[[1]]@annotation, destdir=".")
+mapping <- Table(gpl)[, c("ID", "Gene Symbol")]
+colnames(mapping) <- c("from", "to")
+if(!all(rownames(expr) %in% mapping$from)) {
+    cat("Some probes not in mapping\n")
+    table(rownames(expr) %in% mapping$from)
+    stop("Stopping")
+} else {
+    cat("All probes in mapping\n")
+}
 
-query_df <-
-    AnnotationDbi::select(
-        hgu133plus2.db,
-        keys=keys(hgu133plus2.db,keytype="PROBEID"),
-        columns=c("SYMBOL"),
-        keytype="PROBEID") %>%
-    as_data_frame() %>%
-    set_colnames(c("PROBE", "Hugo")) %>%
-    drop_na()
+microarray_type <- NA
+if(Meta(gpl)$title == "[HG-U133_Plus_2] Affymetrix Human Genome U133 Plus 2.0 Array") {
+  microarray_type <- "Affymetrix HG-U133 Plus 2.0"
+} else if(Meta(gpl)$title == "[HuGene-1_0-st] Affymetrix Human Gene 1.0 ST Array [transcript (gene) version]") {
+  microarray_type <- "Affymetrix Human Gene 1.0 ST Array"
+} else {
+  stop(paste0("Unknown array type ", Meta(gpl)$title))
+}
 
-expr_df <-
-    assayDataElement(gse$GSE20300_series_matrix.txt.gz@assayData, 'exprs') %>% 
-    matrix_to_df("PROBE") %>%
-    inner_join(query_df) %>%
-    dplyr::select(Hugo, everything()) %>%
-    dplyr::select(-PROBE) %>%
-    group_by(Hugo) %>%
-    summarise_all(mean) %>%
-    drop_na() %>%
-    filter(Hugo != "") %>% 
-    gather(key = "sample", value = "expr", -Hugo)
+## Confirm that these data are gcRMA-normalized data--i.e., in log2 space
+data.processing <-
+    gse_object %>%
+    extract2(1) %>%
+    phenoData() %>%
+    pData() %>%
+    dplyr::pull(data_processing) %>%
+    unique() %>%
+    as.character()
+
+cat(paste0(dataset, " data processing: ", data.processing))
+if(data.processing %in% c("gcRMA", "RMA")) {
+  cat(paste0("Based on ", data.processing, " processing, assuming data are in log2 space\n"))
+} else {
+  stop(paste0("Data were not processed via RMA, but with ", data.processing, ".  Can not confirm in log2 space\n"))
+}
+
+expr_df <- expr %>%
+    compressGenes(mapping, fun = max) %>%
+    matrix_to_df("Hugo") %>%
+    gather(key = "sample", value = "expr", - Hugo) %>%
+    filter(Hugo != "")
 
 samples_in_common <- 
     purrr::reduce(list(expr_df$sample, ground_truth_df$sample, annotation_df$sample, translation_df$sample),
            intersect)
 
+expr <- expr[, colnames(expr) %in% samples_in_common]
 
-activity_obj <- Activity(
-    name = "create",
-    description = "process GEO data into usable tables",
-    used = list(ground_truth_id),
-    executed = list("https://github.com/Sage-Bionetworks/Tumor-Deconvolution-Challenge/blob/master/analysis/GSE20300/create_processed_tables.R")
+log_expr_df <- expr_df %>% 
+    filter(sample %in% samples_in_common) %>% 
+    spread(key = "sample", value = "expr")
+
+linear_expr_df <- log_expr_df %>%
+    df_to_matrix("Hugo") %>%
+    raise_to_power(x=2, power=.) %>%
+    matrix_to_df("Hugo")
+
+ground_truth_df <- ground_truth_df %>%
+    filter(sample %in% samples_in_common)
+
+annotation_df <- annotation_df %>%
+    filter(sample %in% samples_in_common)
+
+unity <- 100
+eps <- unity * 0.01
+
+flag <- abs(rowSums(ground_truth_df[, !(colnames(ground_truth_df) == "sample")]) - unity) > eps
+if(any(flag)) {
+    cat("The following rows in the ground truth do not sum to one:\n")
+    print(ground_truth_df[flag,,drop=F])
+}
+
+ground_truth_df <- ground_truth_df %>%
+    df_to_matrix("sample") %>%
+    divide_by(rowSums(.)) %>%
+    matrix_to_df("sample")
+
+expression_manifest_df <- tibble(
+    path = c("expression_log.tsv", "expression_linear.tsv", "expression_log_probe.tsv"),
+    parent = upload_id,
+    executed = script_url,
+    activityName = activity_name,
+    dataset = dataset,
+    used = used,
+    file_type = "expression",
+    expression_type = "microarray",
+    microarray_type = microarray_type,
+    probe_space = c("gene", "gene", "probe"),
+    expression_space = c("log2", "linear", "log2")
 )
 
+annotation_manifest_df <- tibble(
+    path = "annotation.tsv",
+    parent = upload_id,
+    executed = script_url,
+    activityName = activity_name,
+    dataset = dataset,
+    used = used,
+    file_type = "annotations",
+    annotations = str_c(colnames(annotation_df)[-1], collapse = ";")
+)
 
-write_tsv(expr_df, "expression_affy.tsv")
+ground_truth_manifest_df <- tibble(
+    path = "ground_truth.tsv",
+    parent = gt_upload_id,
+    executed = script_url,
+    activityName = activity_name,
+    dataset = dataset,
+    used = used,
+    file_type = "ground truth",
+    unit = "fraction",
+    cell_types = str_c(colnames(ground_truth_df)[-1], collapse = ";")
+)
+
+write_tsv(expr, "expression_log_probe.tsv")
+write_tsv(log_expr_df, "expression_log.tsv")
+write_tsv(linear_expr_df, "expression_linear.tsv")
 write_tsv(annotation_df, "annotation.tsv")
 write_tsv(ground_truth_df, "ground_truth.tsv")
-write_tsv(translation_df, "translation.tsv")
 
-upload_file_to_synapse("expression_affy.tsv", upload_id, activity_obj = activity_obj)
-upload_file_to_synapse("annotation.tsv", upload_id, activity_obj = activity_obj)
-upload_file_to_synapse("ground_truth.tsv", gt_upload_id, activity_obj = activity_obj)
-upload_file_to_synapse("translation.tsv", upload_id, activity_obj = activity_obj)
+write_tsv(expression_manifest_df, "expression_manifest.tsv")
+write_tsv(annotation_manifest_df, "annotation_manifest.tsv")
+write_tsv(ground_truth_manifest_df, "ground_truth_manifest.tsv")
+
+syncToSynapse("expression_manifest.tsv")
+syncToSynapse("annotation_manifest.tsv")
+syncToSynapse("ground_truth_manifest.tsv")
 
