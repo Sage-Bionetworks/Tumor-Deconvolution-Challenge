b9e774a4504bd42ed7ad83388c1be82d5a186b5b
Brian White <brian.white@sagebase.org>
2019-02-11 16:51:55 -0800

    MCP RNA-seq passed QC

--- analysis/MCP-counter-RNA-seq/deconvolve.R
+++ analysis/MCP-counter-RNA-seq/deconvolve.R
@@ -1,48 +1,32 @@
-library(synapser)
+library(synapseClient)
 library(tidyverse)
 library(data.table)
 library(magrittr)
-library(MCPcounter)
+library(stringr)
 
+repo_dir  <- "../../../Tumor-Deconvolution-Challenge/"
+tmp_dir   <- tempdir()
+tmp_dir <- "."
+## expr_id is GSEXXX/pre-processed/
+## This expression file should be in linear/non-log space!
+expr_id   <- "syn18210919"
+## upload_id is GSEXXX/deconvolution_results/
+upload_id <- "syn18212046"
 
+dataset <- "MCP-counter-RNA-seq"
+github.path <- "https://github.com/Sage-Bionetworks/Tumor-Deconvolution-Challenge/blob/master/analysis/"
+dataset.path <- paste0(github.path, dataset)
+deconvolve.url <- paste0(dataset.path, "/deconvolve.R")
 
-expr_id       <- "syn18210919"
-upload_id     <- "syn18212046"
+source(str_c(repo_dir, "scripts/synapseClient_functions.R"))
+source(str_c(repo_dir, "scripts/matrix_functions.R"))
+source(str_c(repo_dir, "scripts/deconvolution-utils.R"))
+setwd(tmp_dir)
+synapseLogin()
 
-dataset       <- "MCP-counter-RNA-seq"
-github.path   <- "https://github.com/Sage-Bionetworks/Tumor-Deconvolution-Challenge/blob/master/analysis/"
-dataset.path  <- paste0(github.path, dataset)
-script_url    <- paste0(dataset.path, "/create_processed_tables.R")
+paths <- download.deconvolution.tools.and.matrices()
+cs.cwltool <- paths$cs.cwltool
+cs.ref.matrix <- paths$cs.ref.matrix
+mcp.cwltool <- paths$mcp.cwltool
 
-source("../../../irwg/iatlas-tool-cibersort/CIBERSORT.R")
-lm22 <- "../../../irwg/iatlas-tool-cibersort/LM22.tsv"
-source("../../scripts/utils.R")
-synLogin()
-
-
-
-expr_df <- expr_id %>% 
-    create_df_from_synapse_id 
-
-write_tsv(expr_df, "expr.tsv")
-
-
-CIBERSORT(lm22, "expr.tsv", QN = F) %>% 
-    as.data.frame %>% 
-    tibble::rownames_to_column("sample") %>% 
-    readr::write_tsv("cibersort_results.tsv")
-
-expr_df %>% 
-    df_to_matrix("Hugo") %>% 
-    MCPcounter.estimate(featuresType = "HUGO_symbols") %>% 
-    write.table("mcpcounter_results.tsv", sep = "\t")
-
-activity_obj <- Activity(
-    name = "create",
-    description = "create and upload deconvolution results using cibersort and mcpcounter",
-    used = list(expr_id),
-    executed = list(script_url)
-)
-
-upload_file_to_synapse("cibersort_results.tsv", upload_id, activity_obj = activity_obj)
-upload_file_to_synapse("mcpcounter_results.tsv", upload_id, activity_obj = activity_obj)
+run.deconvolution.tools(expr_id, upload_id, cs.cwltool, cs.ref.matrix, mcp.cwltool, exec.url = deconvolve.url)
