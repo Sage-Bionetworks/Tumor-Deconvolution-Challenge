26eb48c2ec945aab74e121ca165e36490d7c7280
Ubuntu <ubuntu@ip-172-31-11-157.ec2.internal>
2019-02-14 22:41:21 +0000

    SDY364: modified create_processed_tables to output flow-based and lab-based ground truth

---
+++ analysis/SDY364/setup.R
@@ -0,0 +1,14 @@
+dataset <- "SDY364"
+
+## deconvolution_results_folder_upload_id is GSEXXX/deconvolution_results/
+deconvolution_results_folder_upload_id <- "syn17974650"
+
+## preprocessed_folder_upload_id is GSEXXX/pre-processed
+preprocessed_folder_upload_id     <- "syn17971758"
+
+## gt_upload_id is GSEXXX/
+gt_upload_id  <- "syn17971750"
+
+## wiki_synid is GSEXXX/
+wiki_synid <- "syn17971750"
+
