1a23f94b36c05878ab47edacfadea97acd80b3ee
Brian White <bwhite@ip-10-23-20-138.ec2.internal>
2019-07-25 14:38:30 +0000

    updating SDY113

--- analysis/SDY113/create_challenge_files.R
+++ analysis/SDY113/create_challenge_files.R
@@ -135,6 +135,7 @@ if(!is.na(normalizing.population)) {
   population.value.col <- "population_fraction"
 }
 
+
 sample.mapping <- dataset %>%
   get.immunespace.expression.metadata(.)
 
@@ -161,21 +162,10 @@ cols <- c("participant_id", "population_name_reported", population.value.col)
 gt.mat.raw <- unique(ground_truth_df[, cols]) %>%
     acast(., formula = participant_id ~ population_name_reported)
 
-gsms <- get.immunespace.gsms(dataset)
-
-## gse.ids <- unique(unlist(llply(gsms, .parallel = TRUE, .fun = function(gsm.id) get.gse(gsm.id))))
-gse.ids <- get.gses(gsms, sql.dir = "../")
-names(gse.ids) <- gse.ids
-
-## Get the GEO expression matrices 
-gses <- llply(gse.ids, .parallel = TRUE, .fun = function(gse.id) getGEO(gse.id, GSEMatrix=TRUE))
-gses <- unlist(gses)
-expr.mats <- llply(gses, .fun = function(gse) exprs(gse) %>% as.data.frame)
-
-## Combine the GEO expression matrices into one matrix
-expr.mat <- Reduce("cbind", expr.mats)
-
-expr.mat <- drop.duplicate.columns(expr.mat)
+## Create a data frame holding the ground truth with columns sample, cell.type, and measured
+gt.df <- gt.mat.raw %>%
+  melt() %>%
+  set_colnames(c("sample", "cell.type", "measured"))
 
 sample.mapping <- subset(sample.mapping, participant_id %in% ground_truth_df$participant_id)
 sample.mapping <- subset(sample.mapping, geo_accession %in% colnames(expr.mat))
@@ -193,13 +183,25 @@ if(any(duplicated(sample.mapping$geo_accession))) {
 }
 
 expr.mat <- expr.mat[, sample.mapping$geo_accession]
-gt.mat.raw <- gt.mat.raw[sample.mapping$participant_id, ]
-rownames(gt.mat.raw) <- sample.mapping$geo_accession
+gt.df <- subset(gt.df, sample %in% sample.mapping$participant_id)
+gt.df <- merge(gt.df, sample.mapping[, c("participant_id", "geo_accession")], by.x = c("sample"), by.y = c("participant_id")) %>%
+  dplyr::select("sample" = "geo_accession", "cell.type", "measured") 
 
-## Obfuscate the sample names
-sample.mapping$id <- paste0("S", 1:nrow(sample.mapping))
-rownames(gt.mat.raw) <- sample.mapping$id
-colnames(expr.mat) <- sample.mapping$id
+gsms <- get.immunespace.gsms(dataset)
+
+## gse.ids <- unique(unlist(llply(gsms, .parallel = TRUE, .fun = function(gsm.id) get.gse(gsm.id))))
+gse.ids <- get.gses(gsms, sql.dir = "../")
+names(gse.ids) <- gse.ids
+
+## Get the GEO expression matrices 
+gses <- llply(gse.ids, .parallel = TRUE, .fun = function(gse.id) getGEO(gse.id, GSEMatrix=TRUE))
+gses <- unlist(gses)
+expr.mats <- llply(gses, .fun = function(gse) exprs(gse) %>% as.data.frame)
+
+## Combine the GEO expression matrices into one matrix
+expr.mat <- Reduce("cbind", expr.mats)
+
+expr.mat <- drop.duplicate.columns(expr.mat)
 
 ## May need to update the following get.geo.platform.name function
 platform <- get.geo.platform.name(gses)
@@ -208,14 +210,38 @@ data.processing <- unlist(get.geo.data.processing(gses))
 print(data.processing)
 ## scale <- get.log.or.linear.space(data.processing)
 ## Update the following based on data.processing:
-scale <- "Log"
+normalization <- "RMA"
+scale <- "Log2"
 ## Update the following:
 native.probe.type <- "Probe"
 
+set.seed(1234)
 obfuscated.dataset <- paste0("DS", sum(utf8ToInt(dataset)))
 
-gt.mat.fine <- combine.columns(gt.mat.raw, fine.grained.definitions)
-gt.mat.coarse <- combine.columns(gt.mat.raw, coarse.grained.definitions)
+## Obfuscate the sample names
+obfuscate.sample.names <- TRUE
+
+ret <- subset.and.rename.samples(expr.mat, gt.df, obfuscate.sample.names)
+expr.mat <- ret[["expr"]]
+gt.df <- ret[["gt"]]
+samples.map <- ret[["map"]]
+
+## Spread ground truth into a matrix
+gt.mat.raw <- gt.df %>%    
+    spread(key = "cell.type", value = "measured") %>%
+    dplyr::rename(sample.id = sample)
+
+## Rename and combine ground truth columns into those required for
+## coarse- and fine-grained challenges.
+gt.df.coarse <- NA
+if(length(coarse.grained.definitions) > 0) {
+  gt.df.coarse <- map.and.format.populations(gt.mat.raw, coarse.grained.definitions)
+}
+
+gt.df.fine <- NA
+if(length(fine.grained.definitions) > 0) {
+  gt.df.fine <- map.and.format.populations(gt.mat.raw, fine.grained.definitions)
+}
 
 ## Extract mappings from probe to gene symbol and Ensembl ID.
 probe.to.symbol.map <- get.probe.to.symbol.map(gses)
@@ -223,92 +249,41 @@ probe.to.ensg.map <- get.probe.to.ensg.map(gses)
 
 ## Collapse probesets to gene symbol and Ensembl ID.
 ## Represent a gene by the corresponding probe with largest MAD.
+## Translate probes to symbols and to ensembl IDs
+## compression.fun <- "choose.max.mad.row"
+compression.fun <- "colMeans"
+## compression.fun <- "choose.max.row"
+symbol.compression.fun <- compression.fun
+ensg.compression.fun <- compression.fun
 expr.mat.symbol <- aggregate_rows(expr.mat, subset(probe.to.symbol.map, from %in% rownames(expr.mat)),
-                                  fun = choose.max.mad.row, parallel = TRUE)
+                                  fun = symbol.compression.fun, parallel = TRUE)
 
 expr.mat.ensg <- aggregate_rows(expr.mat, subset(probe.to.ensg.map, from %in% rownames(expr.mat)),
-                                fun = choose.max.mad.row, parallel = TRUE)
+                                fun = ensg.compression.fun, parallel = TRUE)
 
 expr.mats <- list("native" = expr.mat, "ensg" = expr.mat.ensg, "hugo" = expr.mat.symbol)
-nms <- names(expr.mats)
-names(nms) <- nms
-expr.mat.files <- llply(nms, .fun = function(nm) paste0(dataset, "-", nm, "-gene-expr.csv"))
-
-expr.mat.synIds <-
-  llply(nms,
-        .fun = function(nm) {
-                 file <- expr.mat.files[[nm]]
-	         mat <- expr.mats[[nm]]
-	         write.table(file = file, mat, sep = ",", col.names = TRUE, row.names = TRUE, quote = FALSE)
-    	         f <- File(file, parentId = output.folder.synId, synapseStore = TRUE)
-                 ss <- synStore(f, executed = script_url, forceVersion = FALSE)
-                 synId <- get.synapse.id(ss)
-	         synId
-	       })
-
-gt.mats <- list("fine" = gt.mat.fine, "coarse" = gt.mat.coarse)
-nms <- names(gt.mats)
-names(nms) <- nms
-gt.mat.files <- llply(nms, .fun = function(nm) paste0(dataset, "-", nm, "-gt.tsv"))
-
-gt.mat.synIds <-
-  llply(nms,
-        .fun = function(nm) {
-                 file <- gt.mat.files[[nm]]
-	         mat <- gt.mats[[nm]]
-	         write.table(file = file, mat, sep = "\t", col.names = TRUE, row.names = TRUE, quote = FALSE)
-    	         f <- File(file, parentId = output.folder.synId, synapseStore = TRUE)
-                 ss <- synStore(f, executed = script_url, forceVersion = FALSE)
-                 synId <- get.synapse.id(ss)
-	         synId
-	       })
-
+gt.mats <- list("fine" = gt.df.fine, "coarse" = gt.df.coarse)
 mapping.mats <- list("symbol" = probe.to.symbol.map, "ensg" = probe.to.ensg.map)
-nms <- names(mapping.mats)
-names(nms) <- nms
-mapping.mat.files <- llply(nms, .fun = function(nm) paste0(dataset, "-", nm, "-to-native-mapping.tsv"))
-
-mapping.mat.synIds <-
-  llply(nms,
-        .fun = function(nm) {
-                 file <- mapping.mat.files[[nm]]
-	         mat <- mapping.mats[[nm]]
-	         write.table(file = file, mat, sep = "\t", col.names = TRUE, row.names = FALSE, quote = FALSE)
-    	         f <- File(file, parentId = output.folder.synId, synapseStore = TRUE)
-                 ss <- synStore(f, executed = script_url, forceVersion = FALSE)
-                 synId <- get.synapse.id(ss)
-	         synId
-	       })
 
 metadata <-
   list("dataset.name" = obfuscated.dataset,
        "orig.dataset.name" = dataset,
-       "native.expr.file" = expr.mat.files[["native"]],
-       "native.expr.synId" = expr.mat.synIds[["native"]],
-       "hugo.expr.file" = expr.mat.files[["hugo"]],
-       "hugo.expr.synId" = expr.mat.synIds[["hugo"]],       
-       "ensg.expr.file" = expr.mat.files[["ensg"]],
-       "ensg.expr.synId" = expr.mat.synIds[["ensg"]],
        "cancer.type" = cancer.type,
        "platform" = platform,
        "scale" = scale,
        "native.probe.type" = native.probe.type,
        "data.processing" = data.processing,
-       "coarse.gt.file" = gt.mat.files[["coarse"]],
-       "coarse.gt.synId" = gt.mat.synIds[["coarse"]],
-       "fine.gt.file" = gt.mat.files[["fine"]],
-       "fine.gt.synId" = gt.mat.synIds[["fine"]],
-       "symbol.to.native.mapping.file" = mapping.mat.files[["symbol"]],
-       "symbol.to.native.mapping.synId" = mapping.mat.synIds[["symbol"]],       
-       "ensg.to.native.mapping.file" = mapping.mat.files[["ensg"]],
-       "ensg.to.native.mapping.synId" = mapping.mat.synIds[["ensg"]])
+       "normalization" = normalization,
+       "symbol.compression.function" = symbol.compression.fun,
+       "ensg.compression.function" = ensg.compression.fun)
 
-metadata.df <- data.frame("key" = names(metadata), "value" = as.character(metadata))
-
-file <- paste0(dataset, "-metadata.tsv")
-mat <- metadata.df
-write.table(file = file, mat, sep = "\t", col.names = TRUE, row.names = FALSE, quote = FALSE)
-f <- File(file, parentId = output.folder.synId, synapseStore = TRUE)
-ss <- synStore(f, executed = script_url, forceVersion = FALSE)
-synId <- get.synapse.id(ss)
+identifier <- dataset
+if(obfuscate.sample.names) {
+  identifier <- obfuscated.dataset
+}
+## NB: the name of the metadata file uses (and _must_ use) the original dataset name
+metadata.file.name <- paste0(dataset, "-metadata.tsv")
+upload.data.and.metadata.to.synapse(identifier, expr.mats, gt.mats, mapping.mats, metadata,
+                                    output.folder.synId, metadata.file.name,
+                                    executed = script_url, used = NULL, sample.mapping = samples.map)
 
