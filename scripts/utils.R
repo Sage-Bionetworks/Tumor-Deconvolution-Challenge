a6fbbfff68492f3de786fe5452f641a23a7bee3c
Ubuntu <ubuntu@ip-172-31-11-157.ec2.internal>
2019-09-25 22:56:09 +0000

    added novaseq 6000

--- scripts/utils.R
+++ scripts/utils.R
@@ -442,6 +442,8 @@ translate.platform.name <- function(platform.name) {
     platform.name <- "Illumina HiSeq 2000"
   } else if(platform.name == "Illumina HiSeq 4000 (Homo sapiens)") {
     platform.name <- "Illumina HiSeq 4000"
+  } else if(platform.name =- "Illumina NovaSeq 6000 (Homo sapiens)") {
+    platform.name <- "Illumina NovaSeq 6000"
   } else if(platform.name == "Illumina NextSeq 500 (Homo sapiens)") {
     platform.name <- "Illumina NextSeq 500"      
   } else {
