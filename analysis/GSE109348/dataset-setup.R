adda0f51d816f5cb01db0b12877530322499d863
andrewelamb <andrewelamb@gmail.com>
2019-09-26 11:53:59 -0700

    add new dataset

---
+++ analysis/GSE109348/dataset-setup.R
@@ -0,0 +1,10 @@
+
+source("../setup.R")
+
+dataset        <- "GSE109348"
+coarse_mix_id  <- "syn20816229"
+fine_mix_id    <- "syn20816228"
+
+dataset_id <- "syn20816227"
+staging.folder.synId <- "syn20188706"
+
