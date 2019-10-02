223e0856dabff07c8bb4291e524dd51d757e4de0
andrewelamb <andrewelamb@gmail.com>
2019-10-02 13:13:05 -0700

    new dataset

---
+++ analysis/GSE118165/dataset-setup.R
@@ -0,0 +1,9 @@
+
+source("../setup.R")
+
+dataset        <- "GSE118165"
+expr_id        <- "syn20825415"
+coarse_mix_id  <- "syn20825382"
+fine_mix_id    <- "syn20825381"
+dataset_id     <- "syn20825293"
+
