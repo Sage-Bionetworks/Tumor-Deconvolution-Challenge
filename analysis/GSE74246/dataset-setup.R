8041d1f20bad8cb2824426e8f4089192ae34515a
andrewelamb <andrewelamb@gmail.com>
2019-10-08 12:19:29 -0700

    new dataset

---
+++ analysis/GSE74246/dataset-setup.R
@@ -0,0 +1,9 @@
+
+source("../setup.R")
+
+dataset        <- "GSE74246"
+expr_id        <- "syn20833024"
+coarse_mix_id  <- "syn20825384"
+fine_mix_id    <- "syn20825383"
+dataset_id     <- "syn20825298"
+
