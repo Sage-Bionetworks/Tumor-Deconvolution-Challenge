3bfb09a2ef4e260f960f4f08144dbe589ce49420
Brian White <brian.white@sagebase.org>
2020-09-01 12:13:48 -0700

    Use new cibersortX results in sensitivity analysis; change formatting

--- analysis/validation-analysis/plot-limit-of-detection.R
+++ analysis/validation-analysis/plot-limit-of-detection.R
@@ -11,17 +11,31 @@ suppressPackageStartupMessages(p_load(ggpubr))
 suppressPackageStartupMessages(p_load(grid))
 suppressPackageStartupMessages(p_load(ggbeeswarm))
 
+sig.pval.cutoff <- 0.01
+plot.pval.cutoff <- sig.pval.cutoff
+
 synLogin()
 synId <- "syn21739521"
-cat(paste0("NB: using Version 2 here! Don't know how these versions got screwed up!\n")
+cat(paste0("NB: using Version 2 here! Don't know how these versions got screwed up!\n"))
 in.silico.results.file <- synGet(synId, downloadFile = TRUE, version = 2)$path
 res <- read.table(in.silico.results.file, sep=",", header=TRUE, as.is=TRUE, stringsAsFactors = FALSE)
 
 ## Read in the cibersortX results
-synId <- "syn22331779"
-csx.in.silico.results.file <- synGet(synId, downloadFile = TRUE)$path
-csx.res <- read.table(csx.in.silico.results.file, sep="\t", header=TRUE, as.is=TRUE, stringsAsFactors = FALSE)
+## synId <- "syn22331779"
+## csx.in.silico.results.file <- synGet(synId, downloadFile = TRUE)$path
+## csx.res <- read.table(csx.in.silico.results.file, sep="\t", header=TRUE, as.is=TRUE, stringsAsFactors = FALSE)
 
+## Read in the fine-grained cibersortX results
+synId <- "syn22340322"
+fine.csx.in.silico.results.file <- synGet(synId, downloadFile = TRUE)$path
+fine.csx.res <- read.table(fine.csx.in.silico.results.file, sep="\t", header=TRUE, as.is=TRUE, stringsAsFactors = FALSE)
+
+## Read in the coarse-grained cibersortX results
+synId <- "syn22340351"
+coarse.csx.in.silico.results.file <- synGet(synId, downloadFile = TRUE)$path
+coarse.csx.res <- read.table(coarse.csx.in.silico.results.file, sep="\t", header=TRUE, as.is=TRUE, stringsAsFactors = FALSE)
+
+csx.res <- rbind(fine.csx.res, coarse.csx.res)
 
 source("../utils.R")
 
@@ -33,8 +47,35 @@ res <- res[is.na(res$tumor.type),]
 
 csx.res <- subset(csx.res, dataset.name %in% res$dataset.name)
 
-csx.res <- merge(csx.res, unique(res[, c("dataset.name", "sample.id", "subchallenge", "mixture.type", "cell.type", "measured")]))
+merge.measured.manually <- TRUE
+if(merge.measured.manually) {
+    synId <- "syn21763908"
+    coarse.gt.file <- synGet(synId, downloadFile = TRUE)$path
+    coarse.gt <- read.table(coarse.gt.file, sep=",", header=TRUE, as.is=TRUE, stringsAsFactors = FALSE)
+    coarse.gt$subchallenge <- "coarse"
+
+    synId <- "syn21763907"
+    fine.gt.file <- synGet(synId, downloadFile = TRUE)$path
+    fine.gt <- read.table(fine.gt.file, sep=",", header=TRUE, as.is=TRUE, stringsAsFactors = FALSE)
+    fine.gt$subchallenge <- "fine"
+
+    gt <- rbind(coarse.gt, fine.gt)
+    gt <- gt[, c("dataset.name", "sample.id", "subchallenge", "sample", "measured")]
+    colnames(gt) <- c("dataset.name", "sample.id", "subchallenge", "cell.type", "measured")
+    csx.res <- merge(csx.res, unique(gt))
+
+
+} else {
+    ## Merge the measured/ground truth into CIBERSORTx predictions from those
+    ## measured values already included in the predictions for the
+    ## non-CIBERSORTx methods
+    csx.res <- merge(csx.res, unique(res[, c("dataset.name", "sample.id", "subchallenge", "mixture.type", "cell.type", "measured")]))
+}
+
+csx.res <- merge(csx.res, val.metadata, all.x = TRUE, by = c("dataset.name", "subchallenge"))
 csx.res$repo_name <- "CIBERSORTx"
+
+res <- csx.res
 
 if(FALSE) {
 cols <- intersect(colnames(csx.res), colnames(res))
@@ -176,7 +217,7 @@ spike.in.res[["coarse"]] <-
                   print(df)
                   stop("Got different measured values\n")
               }
-              df$measured <- sum(df$measured)
+              df$measured <- round(sum(df$measured), digits=5)
               df[1,,drop=F]
           })
 
@@ -230,9 +271,24 @@ res.matrices <-
                         cmps <- cmps[cmps$group1 == "0.00e+00",]
                         ret <- cmps[, c("group1", "group2", "p")]
                         min.diff.prop <- 1
-                        if(any(ret$p < 0.01)) {
-                            flag <- ret$p < 0.01
-                            min.diff.prop <- min(as.numeric(ret[flag,"group2"]))
+                        if(any(ret$p < sig.pval.cutoff)) {
+                            use.first.sig.diff <- FALSE
+                            if(use.first.sig.diff) {
+                                ## Take the lowest spike-in level that is significantly different
+                                flag <- ret$p < sig.pval.cutoff
+                                min.diff.prop <- min(as.numeric(ret[flag,"group2"]))
+                            } else {
+                                ## Take the lowest spike-in level such that it and all larger levels are
+                                ## significantly different
+                                ret <- ret[order(as.numeric(ret$group2), decreasing=TRUE),]
+                                for(idx in 1:nrow(ret)) {
+                                    if(all(ret[1:idx, "p"] < sig.pval.cutoff)) {
+                                        min.diff.prop <- as.numeric(ret[idx, "group2"])
+                                    } else {
+                                        break
+                                    }
+                                }
+                            }
                         }
                         data.frame(min.diff.prop = min.diff.prop)
                     })
@@ -254,7 +310,7 @@ all.plots <-
                         .fun = function(res.ds) {
 
                             ## res.ds$label <- formatC(as.numeric(as.character(res.ds$min.diff.prop)), format="e", digits=1)
-                            my.format <- function(x) ifelse(x < 0.01, formatC(x, format="e", digits=1),
+                            my.format <- function(x) ifelse(x < sig.pval.cutoff, formatC(x, format="e", digits=1),
                                                             formatC(x, format="f", digits=2, drop0trailing = TRUE))
                             ## res.ds$label <- formatC(as.numeric(as.character(100 * res.ds$min.diff.prop)), format="e", digits=1)
                             res.ds$label <- my.format(as.numeric(as.character(100 * res.ds$min.diff.prop)))
@@ -352,23 +408,50 @@ l_ply(nms,
                               .variables = c("mixture.type"),
                               .fun = function(df.ds) {
 
-                                  df.ds <- df.ds[order(df.ds$measured),]
-                                  sci <- formatC(as.numeric(as.character(df.ds$measured)), format="e", digits=2)
-                                  df.ds$measured <- factor(sci, levels = unique(sci))
+                                  df.ds <- df.ds[order(as.numeric(df.ds$measured)),]
+                                  ## df.ds$measured <- round(df.ds$measured, digits=3)
+                                  use.scientific.notation <- FALSE
+                                  if(use.scientific.notation) {
+                                      sci <- formatC(as.numeric(as.character(df.ds$measured)), format="e", digits=2)
+                                      df.ds$measured <- factor(sci, levels = unique(sci))
+                                  } else {
+                                      df.ds$measured <- 100 * as.numeric(as.character(df.ds$measured))
+                                      df.ds$measured <- factor(df.ds$measured, levels = unique(df.ds$measured))
+                                  }
                                   cmps <- as.data.frame(compare_means(prediction ~ measured,  data = df.ds))
-                                  cmps <- cmps[cmps$group1 == "0.00e+00",]
-                                  cmps <- cmps[cmps$p < 0.1,]
+
+                                  if(use.scientific.notation) {                                  
+                                      cmps <- cmps[as.character(cmps$group1) == "0.00e+00",]
+                                  } else {
+                                      cmps <- cmps[as.character(cmps$group1) == "0",]
+                                  }
+                                  cmps <- cmps[order(as.numeric(as.character(cmps$group2)), decreasing=FALSE),]
+                                  cmps$indx <- 1:nrow(cmps)
+                                  sig.cmps <- cmps[cmps$p < plot.pval.cutoff,]
                                   my_comparisons <-
-                                      llply(1:nrow(cmps),
-                                            .fun = function(i) c(cmps[i,2], cmps[i,3]))
-                                  
+                                      llply(1:nrow(sig.cmps),
+                                            .fun = function(i) c(sig.cmps[i,2], sig.cmps[i,3]))
+
+                                  ## There are a lot of proportions on the x axis. For visualization purposes,
+                                  ## limit to the 0% spike-in, the 5 below the first that is significant,
+                                  ## and all above the first that is significant
+                                  limit.x.axis <- FALSE
+                                  if(limit.x.axis) {
+                                      orig.cmps <- cmps
+                                      keep.indices <- c(1, max(1, min(sig.cmps$indx)-5):min(sig.cmps$indx), min(sig.cmps$indx):max(cmps$indx))
+                                      keep.cmps <- orig.cmps[orig.cmps$indx %in% unique(keep.indices),]
+                                      flag <- as.numeric(as.character(df.ds$measured)) %in% c(0, as.numeric(as.character(keep.cmps$group2)))
+                                      df.ds <- df.ds[flag,]
+                                  }
                                   
                                   g <- ggplot(data = df.ds, aes(x = measured, y = prediction))
                                   g <- g + geom_boxplot()
-                                  g <- g + stat_compare_means(comparisons = my_comparisons, size = 5)
-                                  g <- g + geom_beeswarm()
+                                  ## Remove label = ..p.signif.. to plot the actual pvalues
+                                  g <- g + stat_compare_means(aes(label = ..p.signif..), comparisons = my_comparisons, size = 5, vjust = 0.5)
+                                  ## g <- g + geom_beeswarm()
                                   g <- g + theme(axis.text.x = element_text(angle = 45, hjust = 1))
-                                  g <- g + xlab("Spike-in Proportion") + ylab("Score")
+                                  g <- g + xlab("Spike-in Percent") + ylab("Score")                                  
+                                  ## g <- g + xlab("Spike-in Proportion") + ylab("Score")
                                   mt <- as.character(df.ds$mixture.type[1])
                                   title <- paste0(mt, " Admixtures")
                                   g <- g + ggtitle(title)
