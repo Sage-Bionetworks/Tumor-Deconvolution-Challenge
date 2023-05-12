ff09175e21b57a4eb2b40feb3043a46b88d57935
Brian White <brian.white@jax.org>
2023-05-12 06:34:29 -0700

    Add timing with objectIds

--- analysis/timing/format-timing-results.R
+++ analysis/timing/format-timing-results.R
@@ -1,3 +1,10 @@
+suppressPackageStartupMessages(library(pacman))
+suppressPackageStartupMessages(p_load(synapser))
+suppressPackageStartupMessages(p_load(plyr))
+suppressPackageStartupMessages(p_load(dplyr))
+
+synLogin()
+
 ## The predictions are those from the 'rerun-validation' / 'post-competitive' phase, i.e.,
 ## where the coarse- and fine-grained challenge use the same data.
 ## This is as opposed to the original competitive phase (against which the
@@ -9,3 +16,4 @@ predictions.synId <- "syn22314641"
 ## Read in the "post-competitive" predictions 
 obj <- synGet(predictions.synId, downloadFile=TRUE)
 res.all <- read.table(obj$path, sep=",", header=TRUE, as.is=TRUE, stringsAsFactors=FALSE)
+
